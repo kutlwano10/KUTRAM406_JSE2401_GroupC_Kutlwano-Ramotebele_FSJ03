5d1a75f4ae6f1b1511cbf7c5eb0bf93eb9404f72
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-10-03 15:19:25 +0200

    i imported firebase query and limit to Limit only to get 20 Products from the database

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,12 +1,17 @@
 import { db } from "@/lib/firebaseConfig";
 import { NextResponse } from "next/server";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query,limit } from "firebase/firestore";
 
 
 export async function GET () {
     try {
+
+
         const productsCollection = collection(db, "products")
-        const productsDoc = await getDocs(productsCollection)
+
+        const productsQuery = query(productsCollection, limit(20))
+
+        const productsDoc = await getDocs(productsQuery)
         const productList = productsDoc.docs.map(doc=> ({
             id: doc.id, 
             ...doc.data()
