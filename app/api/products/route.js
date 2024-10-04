86afd3a71204d3549b6cadd78cab5023f14f7eb1
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-10-04 18:52:58 +0200

    i created a filtering by category method using queries

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,6 +1,6 @@
 import { db } from "@/lib/firebaseConfig";
 import { NextResponse } from "next/server";
-import { collection, getDocs, query, limit, } from "firebase/firestore";
+import { collection, getDocs, query, limit, filter, getDoc, where } from "firebase/firestore";
 
 /**
  * Fetches a list of products from the Firestore database.
@@ -19,6 +19,7 @@ export async function GET(req) {
   try {
     const { searchParams } = new URL(req.url);
     const requestedLimit = searchParams.get("limit");
+    const requestedFilter = searchParams.get("filter");
 
     //This set the limit to any number you want else it sets its default to 20
     const productsLimit = requestedLimit ? parseInt(requestedLimit, 10) : 20;
@@ -28,12 +29,33 @@ export async function GET(req) {
     const productsQuery = query(productsCollection, limit(productsLimit));
 
     const productsSnapshots = await getDocs(productsQuery);
+
+    //Getting all Categories
+    
+    const categoriesCollection = collection(db, "categories");
+
+    let categoriesQuery = categoriesCollection
+
+    if(requestedFilter) {
+        categoriesQuery = query(categoriesCollection, where("type", "==", requestedFilter))
+    }
+
+    const categoriesSnapshots = await getDocs(categoriesQuery);
+
+    const categoryList = categoriesSnapshots.docs.map((doc) => ({
+        id: doc.id,
+      ...doc.data(),
+    }));
+
     const productList = productsSnapshots.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+    console.log("This is category",categoryList);
 
-    return NextResponse.json(productList);
+    
+
+    return NextResponse.json(productList, categoryList);
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch Products" },
