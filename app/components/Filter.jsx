5349499be28468b79038c3bec8cbbdf0aa05b946
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-10-05 16:51:15 +0200

    Filter Bug Fixed, i had to get the categories correct since i was getting an array that contained id & categories of categories

--- app/components/Filter.jsx
+++ app/components/Filter.jsx
@@ -1,5 +1,5 @@
-
-
+import meat from "../public/meat.jpg";
+import Image from "next/image";
 const getCategories = async () => {
   try {
     const res = await fetch(
@@ -10,28 +10,38 @@ const getCategories = async () => {
       throw new Error("Failed to fetch categories");
     }
     const data = await res.json();
-    console.log(data)
+    console.log(data);
     return data;
-    
   } catch (error) {
     console.error("Failed to fetch categories", error);
-    return []; 
+    return [];
   }
 };
 
-const Filter = async() => {
-  const {categories} = await getCategories()
+const Filter = async () => {
+  const data = await getCategories();
+  const categories = data[0]?.categories || [];
+
+  console.log("This is console", categories);
 
   return (
     <div>
-      <h1 className="text-gray-800 text-2xl font-semibold pl-5 mb-4">Categories</h1>
+      <h1 className="text-gray-800 text-2xl font-semibold pl-5 mb-4">
+        Categories
+      </h1>
       <button className="w-20 h-10 bg-[blue] text-white py-2 rounded-md mb-2 mt-4 hover:bg-[blue]">
         All
       </button>
       <div className="flex w-full gap-6 mb-8 lg:justify-center overflow-x-auto">
-        {categories && categories.categories.map((category, index) => (
+        {categories.map((category, index) => (
           <div key={index} className="flex-shrink-0 text-center">
-            <h1 className="mt-2">{category}</h1> {/* Assuming category is just a string */}
+            <Image
+              className="w-14 h-14 object-cover rounded-full mx-auto"
+              src={meat}
+              alt=""
+            />
+            <h1 className="mt-2">{category}</h1>{" "}
+            {/* Assuming category is just a string */}
           </div>
         ))}
       </div>
