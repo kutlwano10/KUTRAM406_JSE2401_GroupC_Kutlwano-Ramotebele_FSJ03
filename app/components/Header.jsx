b95149b31440d567f143486fa5fa39cf18dde374
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-10-10 16:31:00 +0200

    Created a logout and returns the user to be null

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,51 +5,54 @@ import Image from "next/image";
 import cart from "../public/cart.svg";
 import menu from "../public/menu-4.svg";
 import search from "../public/search.svg";
-import logo from "../public/logo.png"
+import logo from "../public/logo.png";
 import InstallPwaButton from "./InstallPwaButton";
+import profile from "../public/profile.svg";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebaseConfig";
 import { useRouter } from "next/navigation";
-
-
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
 
-  const [user] = useAuthState(auth)
-  const router = useRouter()
+  console.log({user})
 
-  if(!user) {
-    router.push('/sign-up')
-  }
+  // if (!user) {
+  //   router.push("/sign-up");
+  // }
 
   return (
     <>
       <header className=" w-full fixed top-0 bg-white shadow-md z-10">
         <nav className="flex justify-between items-center  px-4 md:p-[8%] py-4 md:py-6">
           <Image className="w-7" src={menu} alt="" />
-          <InstallPwaButton/>
+          <InstallPwaButton />
 
-          <Link
-            href="/"
-            className=""
-          >
+          <Link href="/" className="">
             <Image
               className="object-cover w-28"
               priority="true"
               alt="logo"
               src={logo}
-              width=''
-              height=''
-
+              width=""
+              height=""
             />
           </Link>
 
           <div className=" flex items-center  md:gap-4">
             {/* Login */}
-            <Link href="">
-              <Image className=" w-6" src={search} alt="" />
+            <Link
+              className="text-center flex justify-center flex-col items-center"
+              href="/sign-in"
+            >
+              <Image className=" w-6" src={profile} alt="" />
+              {/* {!user ? <p>sign out</p> : <p onClick={router.push('/sign-in')}>Sign In</p>} */}
             </Link>
+
+            <button onClick={() => signOut(auth)}>Sign out</button>
             {/* cart */}
             <button className="relative cursor-pointer">
               {/* {console.log(totalItemsInCart)} */}
